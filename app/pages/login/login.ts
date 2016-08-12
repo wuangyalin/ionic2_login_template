73e5409c30e37c463c457c91997858cb8bb91981
wuangyalin <wuangyalin@gmail.com>
2016-08-12 16:34:45 +1000

    Added Animation
    Added animations

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import { NavController,Alert } from 'ionic-angular';
 import { CreateAccountPage } from '../create-account/create-account';
 
 @Component({
   templateUrl: 'build/pages/login/login.html',
+  animations: [
+    //For the logo
+    trigger('logoState', [
+      state('logoIn', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      transition('void => *', [
+        style({transform: 'translate3d(0,1500px,0'}),
+        animate('2000ms ease-in-out')
+      ])
+    ]),
+    trigger('buttonState', [
+      state('buttonIn', style({
+         opacity: 1
+      })),
+      transition('void => *', [
+        style({opacity: 0}),
+        animate('1000ms 2000ms ease-in')
+      ])
+    ]),
+  ]
 })
 export class LoginPage {
+  private logoState: any = "in";
+
 
   constructor(private nav: NavController) {}
 
